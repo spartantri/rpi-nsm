0cb84204e11b511098b81138663d547f41ac99d5
spartantri <spartantri@users.noreply.github.com>
2017-02-20 22:47:26 +0100

    Update rPi3-ap-setup.sh
    Reworked routing and NAT'ing settings, DNS settings

--- rPi3-ap-setup.sh
+++ rPi3-ap-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This version uses September 2016 rpi jessie image, please use this image
-# based on https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390
+# based on https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390 and http://elinux.org/RPI-Wireless-Hotspot
 
 if [ "$EUID" -ne 0 ]
 	then echo "Must be root"
@@ -25,7 +25,7 @@ if [[ $# -eq 1 ]]; then
 fi
 
 apt-get remove --purge hostapd -y
-apt-get install hostapd dnsmasq -y
+apt-get install -y hostapd dnsmasq udhcpd
 
 cat > /etc/systemd/system/hostapd.service <<EOF
 [Unit]
@@ -81,5 +81,34 @@ EOF
 echo "denyinterfaces wlan1" >> /etc/dhcpcd.conf
 
 systemctl enable hostapd
+
+mv /etc/udhcpd.conf /etc/udhcpd.conf.orig
+cat > /etc/udhcpd.conf <<EOF
+
+start 10.3.2.100 # This is the range of IPs that the hostspot will give to client devices.
+end 10.3.2.250
+interface wlan1 # The device uDHCP listens on.
+remaining yes
+opt dns 216.146.35.35 8.26.56.26 # The DNS servers client devices will use.
+opt subnet 255.255.255.0
+opt router 10.3.2.1 # The Pi's IP address on wlan0 which we will set up shortly.
+opt lease 864000 # 10 day DHCP lease time in seconds
+
+EOF
+
+sed -i -- 's,#DAEMON_CONF.*,DAEMON_CONF="/etc/hostapd/hostapd.conf",g' /etc/default/hostapd
+
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sed -i -- 's,#net.ipv4.ip_forward=.*,net.ipv4.ip_forward=1,g' /etc/sysctl.conf
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan1 -o eth0 -j ACCEPT
+sh -c "iptables-save > /etc/iptables.ipv4.nat"
+echo "up iptables-restore < /etc/iptables.ipv4.nat" >> /etc/network/interfaces
+
+service udhcpd start
+update-rc.d hostapd enable
+update-rc.d udhcpd enable
 
 echo "All done! Please reboot"
